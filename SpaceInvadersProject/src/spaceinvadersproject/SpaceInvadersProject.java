6351c1795be72041a61b39b56f6fd8827d583c4d
Kevin Campos Venegas <54250924+KenderMan@users.noreply.github.com>
2020-11-17 15:57:43 -0300

    limpiar

--- SpaceInvadersProject/src/spaceinvadersproject/SpaceInvadersProject.java
+++ SpaceInvadersProject/src/spaceinvadersproject/SpaceInvadersProject.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spaceinvadersproject;
 
-/**
- *
- * @author kende
- */
-public class SpaceInvadersProject {
+public class SpaceInvadersProject 
+{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        
     }
     
 }
