655a51aff7582dea8ee20d75ec22871df2ec6a25
Kevin Campos Venegas <54250924+KenderMan@users.noreply.github.com>
2020-12-08 19:32:59 -0300

    Reunion_1
    xd

---
+++ SpaceInvadersProject/src/Controlador/Player.java
@@ -0,0 +1,88 @@
+package Controlador;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Ellipse2D;
+import javax.swing.*;
+
+public class Player extends JPanel implements ActionListener, KeyListener
+{
+    Timer timer = new Timer(5, this);
+    double x=0, y=0, speedX=0, speedY=0;
+    
+    public Player(){
+        timer.start();
+        addKeyListener(this);
+        
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+    }
+    
+    public void paintComponent(Graphics grafico){
+        super.paintComponent(grafico);
+        
+        Graphics2D g2d = (Graphics2D)grafico;
+        g2d.fill(new Ellipse2D.Double(x, y, 40, 40));
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+        
+        x += speedX;
+        y += speedY;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void left(){
+        speedY = 0;
+        speedX = -1.5;
+    }
+    
+    public void right(){
+        speedY = 0;
+        speedX = 1.5;
+    }
+    
+    public void up(){
+        speedY = -1.5;
+        speedX = 0;
+    }
+    
+    public void down(){
+        speedY = 1.5;
+        speedX = 0;
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        
+        if (code == KeyEvent.VK_LEFT) {
+            left();
+        }
+        if (code == KeyEvent.VK_RIGHT) {
+            right();
+        }
+        if (code == KeyEvent.VK_UP) {
+            up();
+        }
+        if (code == KeyEvent.VK_DOWN) {
+            down();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- SpaceInvadersProject/src/spaceinvadersproject/SpaceInvadersProject.java
+++ SpaceInvadersProject/src/spaceinvadersproject/SpaceInvadersProject.java
@@ -1,11 +1,22 @@
 package spaceinvadersproject;
 
+import Controlador.Player;
+import javax.swing.JFrame;
+
 public class SpaceInvadersProject 
 {
 
     public static void main(String[] args) 
     {
-        // comentario 1
+        JFrame ventana = new JFrame();
+        
+        Player player = new Player();
+        ventana.add(player);
+        
+        ventana.setSize(800,600);
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
     
 }
